4808c685f58bf56116bd8fa8191568d235afe236
Jonatas Caetano <jonatas.calves@gmail.com>
2022-12-20 20:38:28 -0300

    feat: added the getPost method that returns the PostDetails page

--- src/main/java/com/jonatas/blog/controller/PostController.java
+++ src/main/java/com/jonatas/blog/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.jonatas.blog.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jonatas.blog.model.PostModel;
@@ -21,6 +23,14 @@ public class PostController {
         ModelAndView mv = new ModelAndView("posts");
         List<PostModel> posts = postService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @GetMapping(value = "/posts/{id}")
+    public ModelAndView getPost(@PathVariable UUID id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        PostModel post = postService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 }
